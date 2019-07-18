344a5369775afacda55899f96665a35a467e090b
skiwei <skiwei@yahoo.com>
2019-07-18 13:03:16 -0700

    google map api url

--- resources/views/layouts/partials/gmap3.php
+++ resources/views/layouts/partials/gmap3.php
@@ -1,2 +1,2 @@
-<script type="text/javascript" src="http://maps.google.com/maps/api/js"></script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?key=<?php echo env('GOOGLE_KEY') ?>"></script>
 <script type="text/javascript" src="/js/gmap3.min.js"></script>
