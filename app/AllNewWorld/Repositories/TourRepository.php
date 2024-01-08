d6c3c3f6736351b68f1c4f5b523264c07c63048a
Wilson Yang <skiwei@yahoo.com>
2024-01-08 13:05:04 -0800

    update api url

--- app/AllNewWorld/Repositories/TourRepository.php
+++ app/AllNewWorld/Repositories/TourRepository.php
@@ -1,49 +1,50 @@
 <?php
+
 namespace App\AllNewWorld\Repositories;
 
 use GuzzleHttp\Client;
 
 class TourRepository
 {
-	private $client;
-	
-	public function __construct()
-    {
-    	$this->client = new Client(['base_uri'=>'http://www.sayangholidays.com/api/']);
-    }
-    
-    public function featuredTours($limit)
-    {
-    	$res = $this->client->get('featuredTours/'.$limit);
-    	
-    	return json_decode($res->getBody());
-    }
-    
-    public function getTour($tourId)
-    {
-    	$res = $this->client->get('tours/'.$tourId);
-    	
-    	return json_decode($res->getBody());
-    }
-    
-    public function getDestinations()
-    {
-    	$res = $this->client->get('destinations');
-    	 
-    	return json_decode($res->getBody());
-    }
-    
-    public function getDestination($countryId)
-    {
-    	$res = $this->client->get('destination/'.$countryId);
-    	
-    	return json_decode($res->getBody());
-    }
-    
-    public function getToursByCountry($countryId)
-    {
-    	$res = $this->client->get('toursByCountry/'.$countryId);
-    	 
-    	return json_decode($res->getBody());
-    }
+  private $client;
+
+  public function __construct()
+  {
+    $this->client = new Client(['base_uri' => 'https://www.sayangholidays.net/api/']);
+  }
+
+  public function featuredTours($limit)
+  {
+    $res = $this->client->get('featuredTours/' . $limit);
+
+    return json_decode($res->getBody());
+  }
+
+  public function getTour($tourId)
+  {
+    $res = $this->client->get('tours/' . $tourId);
+
+    return json_decode($res->getBody());
+  }
+
+  public function getDestinations()
+  {
+    $res = $this->client->get('destinations');
+
+    return json_decode($res->getBody());
+  }
+
+  public function getDestination($countryId)
+  {
+    $res = $this->client->get('destination/' . $countryId);
+
+    return json_decode($res->getBody());
+  }
+
+  public function getToursByCountry($countryId)
+  {
+    $res = $this->client->get('toursByCountry/' . $countryId);
+
+    return json_decode($res->getBody());
+  }
 }
